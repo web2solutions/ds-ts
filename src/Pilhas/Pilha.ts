39ac99b716cb3e1719921bf3d2951cf5bb6d91a0
Eduardo Perotta <web2solucoes@gmail.com>
2022-09-08 18:35:43 -0300

    Pilha

--- src/Pilhas/Pilha.ts
+++ src/Pilhas/Pilha.ts
@@ -1,5 +1,5 @@
 export class Pilha {
-    _itens: Array<string|number> = [];
+    private _itens: Array<string|number> = [];
 
     constructor (valoresIniciais: Array<string|number>) {
         if (valoresIniciais) {
@@ -7,20 +7,20 @@ export class Pilha {
         }
     }
 
-    empilhar (valor:string|number): Array<string|number> {
+    public empilhar (valor:string|number): Array<string|number> {
         this._itens.unshift(valor)
         return this._itens
     }
 
-    desempilhar (): string|number|undefined {
+    public desempilhar (): string|number|undefined {
         return this._itens.shift()
     }
 
-    estaVazia (): boolean {
+    public estaVazia (): boolean {
         return !!(this._itens.length === 0)
     }
 
-    topo () {
+    public topo () {
         return this._itens[0]
     }
 }
