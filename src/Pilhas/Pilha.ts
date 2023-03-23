6bb949cfefc56f6c146cecc947a20a7fcd06cb36
José Eduardo Almeida <web2solucoes@gmail.com>
2023-03-22 23:55:42 -0300

    Update Pilha.ts

--- src/Pilhas/Pilha.ts
+++ src/Pilhas/Pilha.ts
@@ -1,41 +1,41 @@
 export class Pilha <T> {
-  private _itens: Array<T> = [];
+  private pilha: Array<T> = [];
   
   constructor (valoresIniciais: Array<T> = []) {
-    this._itens = valoresIniciais;
+    this.pilha = valoresIniciais;
   }
   
   // empilhar item no topo da pilha
   public empilhar (valor:T): Array<T> {
-    this._itens.push(valor);
-    return this._itens;
+    this.pilha.push(valor);
+    return this.pilha;
   }
   
   // tirar o último ítem da lista
   public desempilhar (): T {
-    if (!(this._itens.length > 0)) {
+    if (!(this.pilha.length > 0)) {
       throw new Error('Pilha vazia');
     }
-    return this._itens.pop() as unknown as T;
+    return this.pilha.pop() as unknown as T;
   }
   
   // checa se pilha está vazia
   public get estaVazia (): boolean {
-    return !!(this._itens.length === 0);
+    return !!(this.pilha.length === 0);
   }
   
   // recupera o ítem na base da pilha
   public get base () {
-    return this._itens[0];
+    return this.pilha[0];
   }
   
   // recupera o ítem no topo da pilha
   public get topo () {
-    return this._itens[this._itens.length - 1];
+    return this.pilha[this.pilha.length - 1];
   }
   
   // retorna o tamanho da pilha
   public get tamanho() { 
-    return this._itens.length;
+    return this.pilha.length;
   }
 }
